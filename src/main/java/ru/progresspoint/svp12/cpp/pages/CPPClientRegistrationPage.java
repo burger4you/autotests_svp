0b5c337499dc6f41bf6443ac8c2ea5599adc8b79
Александр Бурдейный <alexander.burdeyniy@gmail.com>
2015-08-19 13:47:03 +0300

    Фикс второго лицевого счета

--- src/main/java/ru/progresspoint/svp12/cpp/pages/CPPClientRegistrationPage.java
+++ src/main/java/ru/progresspoint/svp12/cpp/pages/CPPClientRegistrationPage.java
@@ -223,7 +223,7 @@ public class CPPClientRegistrationPage extends CPPSelectizePageObject {
     @FindBy(id = "client_accounts_attributes_0_name")
     WebElementFacade clientAccountNameField;
 
-    @FindBy(xpath = ".//*[@class='string optional name-account 0 svp-validate-required form-control']")
+    @FindBy(xpath = ".//*[@class='string optional name-account 0 required form-control']")
     WebElementFacade clientSecondAccountNameField;
 
     @FindBy(id = "client_accounts_attributes_0_num")
