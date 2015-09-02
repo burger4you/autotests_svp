ab21b0a996a38d4f73a296be92163a1c610aae85
Александр Бурдейный <alexander.burdeyniy@gmail.com>
2015-09-02 12:18:00 +0300

    Поправил ожидание загрузки главной страницы в ЛК

--- src/main/java/ru/progresspoint/svp12/lk/pages/LKMainPage.java
+++ src/main/java/ru/progresspoint/svp12/lk/pages/LKMainPage.java
@@ -4,6 +4,7 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.At;
+import net.thucydides.core.annotations.WhenPageOpens;
 
 /**
  * Главная страница Личного Кабинета
@@ -11,11 +12,18 @@ import net.thucydides.core.annotations.At;
 @At("#HOST")
 public class LKMainPage extends PageObject {
 
+    LKMainMenu mainMenu;
+
     @FindBy(xpath = ".//*[@class='content dashboard']")
     WebElementFacade contentDashboard;
 
     @FindBy(xpath = ".//*[text()='Баланс лицевого счета:']/span[@class='tab__value']")
     WebElementFacade accountBalanceField;
+
+    @WhenPageOpens
+    public void loading() {
+        mainMenu.loading();
+    }
 
     public String getsCurrentAccountBalance() {
         return accountBalanceField.getText();
