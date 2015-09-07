977ed263783f05fcbb9d046de2129cdd6278f473
Vasilenko Vasiliy <vvasilenko@npoprogress.com>
2015-09-07 18:58:25 +0300

    Добавил исправления в steps для АРМА РСО и Казначейства

--- src/main/java/ru/progresspoint/svp12/treasury/steps/TreasuryOperatorSteps.java
+++ src/main/java/ru/progresspoint/svp12/treasury/steps/TreasuryOperatorSteps.java
@@ -4,6 +4,7 @@ package ru.progresspoint.svp12.treasury.steps;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import ru.progresspoint.svp12.dz.pages.DZLoginPage;
+import ru.progresspoint.svp12.treasury.pages.TreasuryLoginPage;
 
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.name;
@@ -13,7 +14,7 @@ import static org.openqa.selenium.By.name;
  */
 public class TreasuryOperatorSteps extends ScenarioSteps {
 
-    DZLoginPage loginPage;
+    TreasuryLoginPage loginPage;
 
     @Step("Нажимает на ссылку {0}")
     public void clicksToLink(String linkText) {
